6844463d85f61baf6900eac76a32b8ff558d82a2
João Vitor Ribeiro <joaotvrodeio@gmail.com>
2022-01-25 15:11:31 -0300

    Alteração das class para o arquivos index.css

--- src/App.vue
+++ src/App.vue
@@ -38,43 +38,9 @@
           @keypress.enter="addWhats"
           v-mask="['(##) ####-####', '(##) #####-####']"
           placeholder="DDD + Telefone"
-          class="
-            transition
-            ease-in-out
-            duration-300
-            border-2 border-gray-300
-            dark:border-slate-500
-            focus:outline-none focus:ring focus:border-blue-500 focus:bg-white
-            dark:focus:border-blue-500
-            w-56
-            h-12
-            rounded-md
-            text-lg
-            px-4
-            py-2
-            bg-gray-100
-            dark:bg-slate-900
-            text-gray-600
-            dark:text-gray-400
-          "
+          class="inputWhats"
         />
-        <button
-          @click="addWhats"
-          class="
-            bg-green-500
-            p-3
-            rounded-lg
-            ml-2
-            flex
-            justify-center
-            items-center
-            hover:bg-green-600
-            transition
-            ease-in-out
-            duration-300
-            text-white
-          "
-        >
+        <button @click="addWhats" class="btnAdd">
           <span class="font-bold ml-1 flex justify-center items-center">
             <img class="mr-1" src="/mais.svg" alt="" />
             Adicionar</span
