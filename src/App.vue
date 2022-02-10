cb09a5b89480242dbeee701f35b8b3cc6c712420
João Vitor Ribeiro <joaotvrodeio@gmail.com>
2022-02-10 13:02:20 -0300

    Update App.vue

--- src/App.vue
+++ src/App.vue
@@ -1,37 +1,37 @@
 <template>
-  <div class="w-full h-screen bg-slate-900 flex justify-center items-center">
+  <div class="flex items-center justify-center w-full h-screen bg-slate-900">
     <img
       v-if="darkMode == true"
-      class="absolute top-6 right-6 xl:top-9 xl:right-9 cursor-pointer"
+      class="absolute cursor-pointer top-6 right-6 xl:top-9 xl:right-9"
       src="/sun.svg"
       alt=""
       @click="darkFunction"
     />
     <img
       v-else
-      class="absolute top-6 right-6 xl:top-9 xl:right-9 cursor-pointer"
+      class="absolute cursor-pointer top-6 right-6 xl:top-9 xl:right-9"
       src="/moon.svg"
       alt=""
       @click="darkFunction"
     />
     <div
       class="
+        flex flex-col
+        items-center
+        justify-center
+        w-1/2
+        p-8
         bg-white
         dark:bg-slate-800
-        w-1/2
         lg:w-6/12
         md:w-2/3
         h-80
         rounded-xl
-        p-8
-        flex flex-col
-        justify-center
-        items-center
       "
     >
       <img class="w-16" src="/whatsapp.svg" alt="Whatsapp" />
       <p class="text-4xl font-bold text-slate-500 mt-7">Adicionar Contato</p>
-      <div class="flex justify-center items-center mt-8">
+      <div class="flex items-center justify-center mt-8">
         <input
           type="text"
           v-model="telefone"
@@ -41,7 +41,7 @@
           class="inputWhats"
         />
         <button @click="addWhats" class="btnAdd">
-          <span class="font-bold ml-1 flex justify-center items-center">
+          <span class="flex items-center justify-center ml-1 font-bold">
             <img class="mr-1" src="/mais.svg" alt="" />
             Adicionar</span
           >
@@ -98,10 +98,7 @@ export default {
       //TIRA TODOS OS CARACTERES QUE NÃO SÃO NUMERICOS E ESPAÇOS
       let telefone = this.telefone.replace(/\D/g, "");
       /* window.location.href = `https://web.whatsapp.com/send?phone=55${telefone}`; */
-      window.open(
-        `https://web.whatsapp.com/send?phone=55${telefone}`,
-        "_blank"
-      );
+      window.open(`https://web.whatsapp.com/send?phone=55${telefone}`, "self");
       this.telefone = "";
     },
   },
